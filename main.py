1ef8369466c5195f1a8273a098240064a7e56475
MagnoCerebri <76867883+MagnoCerebri@users.noreply.github.com>
2021-01-02 16:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import Flask, jsonify,send_file
 app = Flask(__name__)
-
 @app.route('/')
-def startpage():
-    return 'API for my movie streaming application'
-
-@app.route('/movie')
 def movielist():         
     return send_file('movies.json')
 
