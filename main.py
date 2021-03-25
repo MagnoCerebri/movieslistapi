ea52dabfa5768fe414fa2d85f4ff455cc0dab801
MagnoCerebri <76867883+MagnoCerebri@users.noreply.github.com>
2021-03-25 15:27:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     return "Home page for my movie streaming app api"
+@app.route('/isaac')
+def downloadFile ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = "/TechnoJoJo.jpg"
+    return send_file(path, as_attachment=True)
+@app.route('/tec')
+def downloadFile ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = "/Isaac.jpg"
+    return send_file(path, as_attachment=True)
+
 @app.route('/movies')
 def movielist():         
     return send_file('movies.json')
