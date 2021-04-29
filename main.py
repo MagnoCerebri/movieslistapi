f45506d40f57501e5c11d090702a5c2d766a0740
MagnoCerebri <76867883+MagnoCerebri@users.noreply.github.com>
2021-04-29 14:14:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from flask import Flask, jsonify,send_file
+import json
 app = Flask(__name__)
-@app.route('/')
-def start():
-    return "Home page for my movie streaming app api"
-@app.route('/isaac')
-def downloadFile ():
-    #For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "TechnoJoJo.jpg"
-    return send_file(path, as_attachment=True)
-@app.route('/tec')
-def download():
-    #For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "Isaac.jpg"
-    return send_file(path, as_attachment=True)
-@app.route('/monke')
-def load():
-    #For windows you need to use drive name [ex: F:/Example.pdf]
-    path = "MONKE.jpg"
-    return send_file(path, as_attachment=True)
 
-@app.route('/movies')
+@app.route('/')
 def movielist():         
     return send_file('movies.json')
+
+
+
+@app.route('/search/<string:query>')
+def query(query):
+    with open("movies.json") as jsondata:
+        items = json.load(jsondata)
+        def search_name (name):
+            for keyval in items["movies"]:
+                if name.lower() in keyval['name'].lower():
+                    return keyval
+        if (search_name(query) != None):
+            result={"movies":[
+                search_name(query)
+                 ]
+                }
+            return jsonify(result)
+        else:
+            return jsonify("Movie is not found")
+
+    
+    
+
+
+
 
 if __name__ == "__main__":
     app.run()
