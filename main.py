60e03daf10c87151ceeea46b4276a532a26ccfc4
MagnoCerebri <76867883+MagnoCerebri@users.noreply.github.com>
2021-05-05 15:37:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def query(query):
         
         for keyval in items["movies"]:
                 if query.lower() in keyval['name'].lower():
-                    if len(initial[num])==5:
+                    if len(initial[num])==11:
                         num=num+1
                         initial[num]=[]
                         initial[num].append(keyval)
@@ -35,8 +35,31 @@ def query(query):
        
 
     
-    
+@app.route('/genre/<string:query>')   
+def genre(query):
+    with open("movies.json") as jsondata:
+        items = json.load(jsondata)
+        num=1
+        initial={}
+        initial[num]=[]
+        
+        
+        for keyval in items["movies"]:
+            for i in keyval["genres"]:
+                if query.lower() in i.lower():
+                    if len(initial[num])==11:
+                        num=num+1
+                        initial[num]=[]
+                        initial[num].append(keyval)
+                    else:
+                        initial[num].append(keyval)
+                    
+            
 
+   
+    
+    return initial
+    
 
 
 
